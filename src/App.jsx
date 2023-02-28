bb9627a68c7ca0c7ea2dc2c9242bb2c60925d37e
William Pepple <williampepple1@gmail.com>
2023-02-28 22:00:22 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import {auth} from './firebase'
 import {useAuthState} from 'react-firebase-hooks/auth' 
 import Login from './pages/Login'
 import Homepage from './pages/Homepage'
-import Spinner from 'react-spinkit'
+import { ColorRing } from 'react-loader-spinner'
 
 export default function App() {
   const [user, loading] = useAuthState(auth)
 
   if (loading) {
     return(
-     <Spinner name='circle' color='blue'/>
+     <div className='grid h-screen place-items-center'>
+      <ColorRing
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#b8c480', '#B2A3B5', '#F4442E', '#51E5FF', '#429EA6']}
+        />
+     </div>
     )
   }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -167,7 +167,7 @@ export default function Login() {
               </div>
               <div className="text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                  Password Keeper
+                  Password Kipa
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-300">
                   Store your passwords safely without the fear of it being exposed.
